bbab2222b7f200e479d7435f8ebaf0a3e3acebf3
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-29 10:19:18 +0530

    Create color.hpp

---
+++ .github/workflows/color.hpp
@@ -0,0 +1,15 @@
+namespace TelCoColorCoder
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+}
