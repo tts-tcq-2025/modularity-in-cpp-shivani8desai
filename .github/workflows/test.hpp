caa76638f39d4df0a5e4a9d7b6c6896a95cb9eb7
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-29 10:46:21 +0530

    Create test.hpp

---
+++ .github/workflows/test.hpp
@@ -0,0 +1,5 @@
+#include<iostream>
+
+void testNumberToPair(int,TelCoColorCoder::MajorColor, TelCoColorCoder::MinorColor );
+
+void testPairToNumber(TelCoColorCoder::MajorColor ,TelCoColorCoder::MinorColor, int );
