5a58ac4669d5758ebccdb6b62d360dfc54e37eab
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-29 10:26:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,6 @@
 #include <color.hpp>
 namespace TelCoColorCoder
 {
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
     class ColorPair {
         private:
             MajorColor majorColor;
