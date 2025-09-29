7c36b2f7971b0030da7b03f43b52047669d6c010
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-29 10:21:10 +0530

    Update color.hpp

--- .github/workflows/color.hpp
+++ .github/workflows/color.hpp
@@ -11,5 +11,6 @@ namespace TelCoColorCoder
     const char* MinorColorNames[] = {
         "Blue", "Orange", "Green", "Brown", "Slate"
     };
-    int numberOfMinorColors =
+   int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 }
